8623a5f8942adba25bfe274b238233407f83ab7d
KovalVlada <kovalvlada0910@gmail.com>
2024-12-18 18:50:12 +0200

    hotfix

--- funca.cpp
+++ funca.cpp
@@ -1,6 +1,14 @@
 #include "funca.h"
+#include <cmath>
 
-double FuncAClass::FuncA() {
-    // Placeholder implementation
-    return 0.0;
+double FuncAClass::FuncA(int n, double x) {
+    int terms = 3;  // Fixed to the first 3 terms
+    double result = 0.0;
+
+    for (int i = 0; i < terms; ++i) {
+        double term = pow(-1, i) * pow(x, 2 * i + 1) / factorial(2 * i + 1);
+        result += term;
+    }
+
+    return result;
 }
